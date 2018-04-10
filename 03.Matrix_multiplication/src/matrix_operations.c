c0cd58add1d39fcc67226ad4d9c8b373e89a0c13
Evgenius2020 <spleen47@mail.ru>
2018-04-10 18:05:55 +0700

    03 Less files

--- 03.Matrix_multiplication/src/matrix_operations.c
+++ 03.Matrix_multiplication/src/matrix_operations.c
@@ -1,6 +1,6 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "matrix_operations.h"
-#include "vector_operations.h"
 
 Matrix *create_matrix(unsigned height, unsigned width)
 {
@@ -34,6 +34,18 @@ Matrix *create_transposed_matrix(Matrix *matrix)
     return result;
 }
 
+
+double scalar_vector_x_vector(double *left, double *right, unsigned vector_size)
+{
+    double result = 0;
+    unsigned i;
+    for (i = 0; i < vector_size; i++)
+    {
+        result += left[i] * right[i];
+    }
+    return result;
+}
+
 /// Pre-conditions:
 /// 1) left->width == left->height.
 /// 2) result->height and result->width is initialized.
@@ -55,9 +67,13 @@ void matrix_x_transposed_matrix(Matrix *left, Matrix *transposed_right, Matrix *
 
 void print_matrix(Matrix *matrix)
 {
-    unsigned i;
+    unsigned i, j;
     for (i = 0; i < matrix->height; i++)
     {
-        print_vector(matrix->data + i * matrix->width, matrix->width);
+        for (j = 0; j < matrix->width; j++)
+        {
+            printf("%f ", matrix->data[i * matrix->width + j]);
+        }
+        printf("\n");
     }
 }
